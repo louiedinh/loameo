92e4adc894b909114eb6276a3e51c81c21167abb
Louie Dinh <louiedinh@gmail.com>
2014-05-02 22:02:42 -0700

    Remove files in output

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://pythonpracticeprojects.com'
+SITEURL = 'http://loameo.com'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
